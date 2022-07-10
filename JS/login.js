e6aeb42477b767d0f61dd0ee13104981d9751a21
Fernanda <mariferrm97@hotmail.com>
2022-07-10 15:22:50 +0200

    nuevos cambios

--- JS/login.js
+++ JS/login.js
@@ -1,21 +1,29 @@
 async function login(e){
     e.preventDefault()
 
+    
+
     var email = document.getElementById("email").value;
     var contraseña = document.getElementById("password").value;
     const jsondata = await fetch("http://localhost/scripts-php/Proyecto-Mini-Red-Social-/json/data.json")
     const data = await jsondata.json()
 
-    console.log(userexists(data, email, contraseña))
+    
+    const user = userexists(data, email, contraseña);
 
+    if(user == false){
+        var errorSpan = document.getElementById("formError");
+        errorSpan.innerHTML = "Error en contraseña o nombre de usuario. Por favor revisa y prueba nuevamente" 
+    }
+    
 }
 
-function userexists(data, email, password){
+function userexists(data, email, contraseña){
     for (let i = 0; i < data.length; i++) {
-        if (data[i].email == email && data[i].password == password){
+        if (data[i].email == email && data[i].password == contraseña){
             return true            
         }    
     }
     return false
-
 }
+
