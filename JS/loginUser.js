cc7dff9fc60d07d8b35612c893d571e5721d9f46
Fernanda <mariferrm97@hotmail.com>
2022-07-12 10:33:42 +0200

    cambios errores

--- JS/loginUser.js
+++ JS/loginUser.js
@@ -5,20 +5,19 @@ async function login(e){
     var contraseña = document.getElementById("password").value;
     const jsondata = await fetch("http://localhost/scripts-php/Proyecto-Mini-Red-Social-/json/data.json")
     const data = await jsondata.json()
-    var respuesta = "";
     
     const user = userexists(data, email, contraseña);
     // console.log(`Hola ${user.nombre} fer rompio el codigo`);
 
     if (email == "" || contraseña  == ""){      
-        var respuesta = "Debe rellenar el email y contraseña"
-        console.log(respuesta)
+        var errorSpan = document.getElementById("formError");
+        errorSpan.innerHTML = "<p class='error'>❌​ Debe rellenar el email y contraseña</p>";
     }else if(user){
         var direccion = "../Proyecto-Mini-Red-Social-/pages/perfilpropio.php?email=" + email;
         window.location.href = direccion;
     }else{
         var errorSpan = document.getElementById("formError");
-        errorSpan.innerHTML = "<p class='error'>Error en contraseña o nombre de usuario. Por favor revisa y prueba nuevamente.</p>";
+        errorSpan.innerHTML = "<p class='error'>❌ Error en contraseña o nombre de usuario. Por favor revisa y prueba nuevamente.</p>";
     }
     
 }
